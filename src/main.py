8661354874c8e41979d0759f7ee5da3c566748e8
zvxte <zvxte.codes@gmail.com>
2024-06-28 23:56:58 +0100

    include routers to app

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
+from .routers.v1 import auth as v1_auth, users as v1_users, activities as v1_activities
+
 app = FastAPI()
-
-
-@app.get("/status")
-def status():
-    return {"status_code": 200}
+app.include_router(v1_auth.router)
+app.include_router(v1_users.router)
+app.include_router(v1_activities.router)
