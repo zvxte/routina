6a2a59254807875b75f83f81c23f2ccc1706ba72
zvxte <zvxte.codes@gmail.com>
2024-06-29 05:06:36 +0200

    create api endpoints to manage sessions, create login endpoint

--- src/routers/v1/auth.py
+++ src/routers/v1/auth.py
@@ -1,16 +1,45 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Header
 
 from ...models.user import UserIn, UserOut
+from ...models.session import SessionOut
 
 
 router = APIRouter(prefix="/v1/auth", tags=["auth"])
 
 
 @router.post("/register")
-def register(_user_in: UserIn) -> UserOut:
-    return ...
+def register(user_in: UserIn):
+    """
+    Registers new user.
+    Returns new session ID in response headers.
+    """
+    ...
 
 
 @router.post("/login")
-def login(user_in: UserIn) -> UserOut:
-    return ...
+def login(user_in: UserIn):
+    """
+    Logs existing user in.
+    Returns new session ID in response headers.
+    """
+    ...
+
+
+@router.get("/sessions")
+def get_sessions(session_id: Annotated[str, Header()]) -> list[SessionOut]:
+    """Returns all active sessions"""
+    ...
+
+
+@router.patch("/sessions")
+def patch_session(session_id: Annotated[str, Header()]) -> None:
+    """Updates session expiration time"""
+    ...
+
+
+@router.delete("/sessions")
+def delete_session(session_id: Annotated[str, Header()]) -> None:
+    """Deletes session"""
+    ...
