faec0f0a612407360659cba876db7c0999eceec0
zvxte <zvxte.codes@gmail.com>
2024-06-30 23:46:01 +0100

    create dependencies module

---
+++ src/dependencies.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+
+from fastapi import Cookie, Header
+
+
+class UserAgentHeader:
+    def __init__(self, user_agent: Annotated[str | None, Header()] = None):
+        self.user_agent = user_agent
+
+
+class SessionCookie:
+    def __init__(self, session_id: Annotated[str, Cookie()]):
+        self.session_id = session_id
