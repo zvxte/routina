5a502c6c3871d0eb1b9a02edb63611ae78d61006
zvxte <zvxte.codes@gmail.com>
2024-07-03 07:29:11 +0200

    remove path params, implement get method for users

--- src/routers/v1/users.py
+++ src/routers/v1/users.py
@@ -1,32 +1,37 @@
 from typing import Annotated
 
 from fastapi import APIRouter, status, Depends
+from sqlmodel import Session, select
 
-from ...dependencies import UserPath, SessionCookie
+from ...dependencies import SessionCookie
 from ...models.user import UserOut
+from ...database import engine, Users, Sessions
 
 
 router = APIRouter(prefix="/v1/users", tags=["users"])
 
 
-@router.get("/{user_id}", status_code=status.HTTP_200_OK, response_model=UserOut)
-def get_user(
-    session_cookie: Annotated[SessionCookie, Depends()],
-    user_path: Annotated[UserPath, Depends()],
-) -> UserOut: ...
+@router.get("", status_code=status.HTTP_200_OK, response_model=UserOut)
+async def get_user(session_cookie: Annotated[SessionCookie, Depends()]) -> UserOut:
+    with Session(engine) as session:
+        statement = select(Sessions.user_id).where(
+            Sessions.session_id == session_cookie.session_id
+        )
+        user_id = session.exec(statement).first()
+
+        statement = select(Users).where(Users.user_id == user_id)
+        return session.exec(statement).first()
 
 
 """ TODO """
-# @router.patch("/{user_id}", status_code=status.HTTP_200_OK)
-# def patch_user(
-#     session_cookie: Annotated[SessionCookie, Depends()],
-#     user_path: Annotated[UserPath, Depends()],
+# @router.patch("", status_code=status.HTTP_200_OK)
+# async def patch_user(
+#     session_cookie: Annotated[SessionCookie, Depends()]
 #     #  and some requst body in json
 # ): ...
 
 
-# @router.delete("/{user_id}", status_code=status.HTTP_200_OK)
-# def delete_user(
-#     session_cookie: Annotated[SessionCookie, Depends()],
-#     user_path: Annotated[UserPath, Depends()],
+# @router.delete("", status_code=status.HTTP_200_OK)
+# async def delete_user(
+#     session_cookie: Annotated[SessionCookie, Depends()]
 # ): ...
