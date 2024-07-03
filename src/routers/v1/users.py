49789936e10273e28224777a0e0c8d3f9e8f9897
zvxte <zvxte.codes@gmail.com>
2024-07-02 23:14:58 -0400

    create endpoints structure

--- src/routers/v1/users.py
+++ src/routers/v1/users.py
@@ -1,4 +1,32 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, status, Depends
+
+from ...dependencies import UserPath, SessionCookie
+from ...models.user import UserOut
 
 
 router = APIRouter(prefix="/v1/users", tags=["users"])
+
+
+@router.get("/{user_id}", status_code=status.HTTP_200_OK, response_model=UserOut)
+def get_user(
+    session_cookie: Annotated[SessionCookie, Depends()],
+    user_path: Annotated[UserPath, Depends()],
+) -> UserOut: ...
+
+
+""" TODO """
+# @router.patch("/{user_id}", status_code=status.HTTP_200_OK)
+# def patch_user(
+#     session_cookie: Annotated[SessionCookie, Depends()],
+#     user_path: Annotated[UserPath, Depends()],
+#     #  and some requst body in json
+# ): ...
+
+
+# @router.delete("/{user_id}", status_code=status.HTTP_200_OK)
+# def delete_user(
+#     session_cookie: Annotated[SessionCookie, Depends()],
+#     user_path: Annotated[UserPath, Depends()],
+# ): ...
