db52c83d0e931e789b438bc153309be176183d46
zvxte <zvxte.codes@gmail.com>
2024-07-08 19:19:37 +0200

    add activity path and date dependencies

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import Cookie, Header
+from fastapi import Cookie, Header, Path
+
+from .utils import current_year
 
 
 class UserAgentHeader:
@@ -12,3 +14,23 @@ class UserAgentHeader:
 class SessionCookie:
     def __init__(self, session_id: Annotated[str, Cookie()]):
         self.session_id = session_id
+
+
+class ActivityPath:
+    def __init__(self, activity_id: Annotated[UUID, Path()]):
+        self.activity_id = activity_id
+
+
+class YearPath:
+    def __init__(self, year: Annotated[int, Path(ge=2000, le=current_year())]):
+        self.year = year
+
+
+class MonthPath:
+    def __init__(self, month: Annotated[int, Path(ge=1, le=12)]):
+        self.month = month
+
+
+class DayPath:
+    def __init__(self, day: Annotated[int, Path(ge=1, le=31)]):
+        self.day = day
