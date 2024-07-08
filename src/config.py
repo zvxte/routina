7dec35e2ba1ba89b965f4d7548edc0842cbc1fa6
zvxte <zvxte.codes@gmail.com>
2024-07-08 19:40:10 +0200

    add docs_url to config

--- src/config.py
+++ src/config.py
@@ -4,3 +4,6 @@ DATABASE_ECHO: bool = True  # prints sql queries,
 # how long should session last in seconds
 # 30 days = 30 * 24 * 60 * 60 = 2592000 seconds
 SESSION_DURATION: int = 2592000
+
+# Disable /docs by setting it to None
+DOCS_URL: str | None = "/docs"
--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from .routers.v1 import auth as v1_auth, user as v1_user, activities as v1_activities
 from .middlewares import ValidateSession
+from .config import DOCS_URL
 
 
 app = FastAPI(
     title="routina",
     description="Simple API to track your routines",
+    docs_url=DOCS_URL,
 )
 
 app.include_router(v1_auth.router)
