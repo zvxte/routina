e1a4431e5e8e4a98f95569dbe419b1ef9897b319
zvxte <zvxte.codes@gmail.com>
2024-07-08 19:19:00 +0200

    rename users to user

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 
-from .routers.v1 import auth as v1_auth, users as v1_users, activities as v1_activities
+from .routers.v1 import auth as v1_auth, user as v1_user, activities as v1_activities
 from .middlewares import ValidateSession
 
 
 app = FastAPI()
 
 app.include_router(v1_auth.router)
-app.include_router(v1_users.router)
+app.include_router(v1_user.router)
 app.include_router(v1_activities.router)
 
 app.add_middleware(ValidateSession)
--- src/routers/v1/user.py
+++ src/routers/v1/user.py
@@ -13,6 +13,7 @@ router = APIRouter(prefix="/v1/user", tags=["user"])
 
 @router.get("", status_code=status.HTTP_200_OK, response_model=UserOut)
 async def get_user(session_cookie: Annotated[SessionCookie, Depends()]) -> UserOut:
+    """Returns user data"""
     with Session(engine) as session:
         statement = select(Sessions.user_id).where(
             Sessions.session_id == session_cookie.session_id
@@ -23,15 +24,18 @@ async def get_user(session_cookie: Annotated[SessionCookie, Depends()]) -> UserO
         return session.exec(statement).first()
 
 
-""" TODO """
-# @router.patch("", status_code=status.HTTP_200_OK)
-# async def patch_user(
-#     session_cookie: Annotated[SessionCookie, Depends()]
-#     #  and some requst body in json that needs to change
-# ): ...
+@router.delete("", status_code=status.HTTP_200_OK)
+async def delete_user(session_cookie: Annotated[SessionCookie, Depends()]) -> None:
+    """Deletes user"""
+    with Session(engine) as session:
+        statement = select(Sessions.user_id).where(
+            Sessions.session_id == session_cookie.session_id
+        )
+        user_id = session.exec(statement).first()
 
+        statement = select(Users).where(Users.user_id == user_id)
+        user_entry = session.exec(statement).first()
+        session.delete(user_entry)
+        session.commit()
 
-# @router.delete("", status_code=status.HTTP_200_OK)
-# async def delete_user(
-#     session_cookie: Annotated[SessionCookie, Depends()]
-# ): ...
+    return None
